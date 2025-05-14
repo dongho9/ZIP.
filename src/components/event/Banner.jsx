75fac500c357f259a5bfa3e6639815cd42b56ca2
정휘찬 <jeonghwichan@jeonghwichan-ui-MacBookPro.local>
2025-05-14 18:23:51 +0900

    [휘찬]이벤트페이지 수정

--- src/components/event/Banner.jsx
+++ src/components/event/Banner.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components"
 
 const ImgWrapper = styled.div`
+/* position: relative; */
   width: 100%;
-  height: 600px;
+  img{
+    width: 100%;
+  height: 100%;
   object-fit: cover;
+  }
 `
 
 const Banner = () => {
