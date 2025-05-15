7d7da9804a2f2dedef06d31b0b7d6c919b80fbf3
dayeon123 <choidayeon1230@gmail.com>
2025-05-15 11:23:40 +0900

    ,[다연]test

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Agreement from "../components/signup/Agreement";
-import Modal from "../components/signup/Modal";
+
 const Wrapper = styled.div`
   width: 540px;
   margin: 60px auto;
@@ -26,12 +27,13 @@ const Wrapper = styled.div`
 const Title = styled.h3`
   font-size: 3.6rem;
   font-weight: bold;
+  font-family: "EHNormalTrial";
   @media screen and (max-width: 428px) {
     font-size: 3rem;
   }
 `;
 
-const Form = styled.div`
+const Form = styled.form`
   /* width: 500px; */
   width: 100%;
   height: 100%;
@@ -119,21 +121,76 @@ const Desc = styled.p`
   color: var(--border-color);
 `;
 
+const Error = styled.p`
+  font-size: 1.2rem;
+  color: var(--accent);
+`;
+
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const password = watch("password");
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <Wrapper>
         <Title>JOIN</Title>
-        <Form>
-          <Input type="text" placeholder="이름 *" />
-          <Input type="text" placeholder="아이디 *" />
+        <Form id="form" as="form" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            type="text"
+            placeholder="이름 *"
+            {...register("name", { required: "이름을 입력해주세요." })}
+          />
+          {errors.name && <Error>{errors.name.message}</Error>}
+          <Input
+            type="text"
+            placeholder="아이디 *"
+            {...register("username", {
+              required: "아이디를 입력해주세요.",
+              minLength: {
+                value: 4,
+                message: "아이디는 최소 4자 이상이어야 합니다.",
+              },
+            })}
+          />
+          {errors.username && <Error>{errors.username.message}</Error>}
           <Group>
-            <Input type="password" placeholder="비밀번호 *" />
-            <Desc>
-              (영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 10자~16자)
-            </Desc>
+            <Input
+              type="password"
+              placeholder="비밀번호 *"
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)(?=.{10,16}).*$/,
+                  message:
+                    "비밀번호는 영문 + 숫자 포함, 10자 이상 16자 이하입니다.",
+                },
+              })}
+            />
+            <Desc>(영문 대소문자+숫자 포함, 10자~16자)</Desc>
+            {errors.password && <Error>{errors.password.message}</Error>}
           </Group>
-          <Input type="password" placeholder="비밀번호 확인 *" />
+          <Input
+            type="password"
+            placeholder="비밀번호 확인 *"
+            {...register("confirmPassword", {
+              required: "비밀번호를 다시 한 번 입력해주세요.",
+              validate: (value) =>
+                value === password || "비밀번호가 일치하지 않습니다.",
+            })}
+          />
+          {errors.confirmPassword && (
+            <Error>{errors.confirmPassword.message}</Error>
+          )}
           <InputGroup>
             <Input type="text" placeholder="우편번호" />
             <Button>우편번호 검색</Button>
@@ -141,19 +198,31 @@ const Signup = () => {
           <Input type="text" placeholder="집주소" />
           <Input type="text" placeholder="상세주소" />
           <InputGroup>
-            <Input type="text" placeholder="이메일 *" />
+            <Input
+              type="text"
+              placeholder="이메일 *"
+              {...register("email", {
+                required: "이메일을 입력해주세요.",
+              })}
+            />
+
             <span>@</span>
             <Select name="" id="">
-              <option value="">선택</option>
+              <option value="" selected disabled>
+                선택
+              </option>
               <option value="naver.com">naver.com</option>
               <option value="gmail.com">gmail.com</option>
               <option value="hanmail.net">hanmail.net</option>
               <option value="nate.com">nate.com</option>
             </Select>
           </InputGroup>
+          {errors.email && <Error>{errors.email.message}</Error>}
         </Form>
         <Agreement />
-        <Btn>회원가입 하기</Btn>
+        <Btn type="submit" form="form">
+          회원가입 하기
+        </Btn>
       </Wrapper>
     </div>
   );
