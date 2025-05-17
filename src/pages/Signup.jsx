a927e15ce4367eff2aa4f50943c8f96a4d60c58c
dayeon123 <choidayeon1230@gmail.com>
2025-05-18 00:09:42 +0900

    [다연]회원가입 수정

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Agreement from "../components/signup/Agreement";
+import Address from "../components/signup/Address";
 
 const Wrapper = styled.div`
   width: 540px;
@@ -98,6 +99,7 @@ const Select = styled.select`
 
 const Btn = styled.button`
   width: 100%;
+  font-family: "Pretendard";
   font-size: 1.4rem;
   padding: 24px 0;
   color: var(--light-color);
@@ -106,13 +108,6 @@ const Btn = styled.button`
   cursor: pointer;
   @media screen and (max-width: 428px) {
     padding: 20px 0;
-  }
-`;
-
-const Button = styled(Btn)`
-  width: 44%;
-  @media screen and (max-width: 428px) {
-    width: 50%;
   }
 `;
 
@@ -141,90 +136,82 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <Wrapper>
-        <Title>JOIN</Title>
-        <Form id="form" as="form" onSubmit={handleSubmit(onSubmit)}>
+    <Wrapper>
+      <Title>JOIN</Title>
+      <Form id="form" as="form" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="text"
+          placeholder="이름 *"
+          {...register("name", { required: "이름을 입력해주세요." })}
+        />
+        {errors.name && <Error>{errors.name.message}</Error>}
+        <Input
+          type="text"
+          placeholder="아이디 *"
+          {...register("username", {
+            required: "아이디를 입력해주세요.",
+            minLength: {
+              value: 4,
+              message: "아이디는 최소 4자 이상이어야 합니다.",
+            },
+          })}
+        />
+        {errors.username && <Error>{errors.username.message}</Error>}
+        <Group>
           <Input
-            type="text"
-            placeholder="이름 *"
-            {...register("name", { required: "이름을 입력해주세요." })}
-          />
-          {errors.name && <Error>{errors.name.message}</Error>}
-          <Input
-            type="text"
-            placeholder="아이디 *"
-            {...register("username", {
-              required: "아이디를 입력해주세요.",
-              minLength: {
-                value: 4,
-                message: "아이디는 최소 4자 이상이어야 합니다.",
+            type="password"
+            placeholder="비밀번호 *"
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)(?=.{10,16}).*$/,
+                message:
+                  "비밀번호는 영문 + 숫자 포함, 10자 이상 16자 이하입니다.",
               },
             })}
           />
-          {errors.username && <Error>{errors.username.message}</Error>}
-          <Group>
-            <Input
-              type="password"
-              placeholder="비밀번호 *"
-              {...register("password", {
-                required: "비밀번호를 입력해주세요.",
-                pattern: {
-                  value: /^(?=.*[A-Za-z])(?=.*\d)(?=.{10,16}).*$/,
-                  message:
-                    "비밀번호는 영문 + 숫자 포함, 10자 이상 16자 이하입니다.",
-                },
-              })}
-            />
-            <Desc>(영문 대소문자+숫자 포함, 10자~16자)</Desc>
-            {errors.password && <Error>{errors.password.message}</Error>}
-          </Group>
+          <Desc>(영문 대소문자+숫자 포함, 10자~16자)</Desc>
+          {errors.password && <Error>{errors.password.message}</Error>}
+        </Group>
+        <Input
+          type="password"
+          placeholder="비밀번호 확인 *"
+          {...register("confirmPassword", {
+            required: "비밀번호를 다시 한 번 입력해주세요.",
+            validate: (value) =>
+              value === password || "비밀번호가 일치하지 않습니다.",
+          })}
+        />
+        {errors.confirmPassword && (
+          <Error>{errors.confirmPassword.message}</Error>
+        )}
+        <Address />
+        <InputGroup>
           <Input
-            type="password"
-            placeholder="비밀번호 확인 *"
-            {...register("confirmPassword", {
-              required: "비밀번호를 다시 한 번 입력해주세요.",
-              validate: (value) =>
-                value === password || "비밀번호가 일치하지 않습니다.",
+            type="text"
+            placeholder="이메일 *"
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
             })}
           />
-          {errors.confirmPassword && (
-            <Error>{errors.confirmPassword.message}</Error>
-          )}
-          <InputGroup>
-            <Input type="text" placeholder="우편번호" />
-            <Button>우편번호 검색</Button>
-          </InputGroup>
-          <Input type="text" placeholder="집주소" />
-          <Input type="text" placeholder="상세주소" />
-          <InputGroup>
-            <Input
-              type="text"
-              placeholder="이메일 *"
-              {...register("email", {
-                required: "이메일을 입력해주세요.",
-              })}
-            />
-
-            <span>@</span>
-            <Select name="" id="">
-              <option value="" selected disabled>
-                선택
-              </option>
-              <option value="naver.com">naver.com</option>
-              <option value="gmail.com">gmail.com</option>
-              <option value="hanmail.net">hanmail.net</option>
-              <option value="nate.com">nate.com</option>
-            </Select>
-          </InputGroup>
-          {errors.email && <Error>{errors.email.message}</Error>}
-        </Form>
-        <Agreement />
-        <Btn type="submit" form="form">
-          회원가입 하기
-        </Btn>
-      </Wrapper>
-    </div>
+          <span>@</span>
+          <Select name="" id="">
+            <option value="" selected disabled>
+              선택
+            </option>
+            <option value="naver.com">naver.com</option>
+            <option value="gmail.com">gmail.com</option>
+            <option value="hanmail.net">hanmail.net</option>
+            <option value="nate.com">nate.com</option>
+          </Select>
+        </InputGroup>
+        {errors.email && <Error>{errors.email.message}</Error>}
+      </Form>
+      <Agreement />
+      <Btn type="submit" form="form">
+        회원가입 하기
+      </Btn>
+    </Wrapper>
   );
 };
 
